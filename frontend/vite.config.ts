8587dae718e4f3ae372da6f1dd45d90f4299e046
Sudev V S <sudevvs1999@gmail.com>
2024-08-25 23:45:31 +0530

    package-json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css:{
+  css: {
     postcss: './postcss.config.js',
   },
-  server:{
-    port:3000
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000,
+  },
+  esbuild: {
+    target: 'ES2016',
+    platform: 'browser',
   }
 })
+
