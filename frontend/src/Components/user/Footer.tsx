2363ec78d197b5746ad0eba212d039383ce64cc6
Sudev V S <sudevvs1999@gmail.com>
2024-09-03 18:55:41 +0530

    footer logo, navbar logo updated

--- frontend/src/Components/user/Footer.tsx
+++ frontend/src/Components/user/Footer.tsx
@@ -3,6 +3,7 @@ import { IconType } from 'react-icons'
 import { FaFacebook, FaInstagram } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import logoBlack  from "../../assets/images/logo-black.png"
 
 interface Section {
    title: string;
@@ -18,29 +19,9 @@ interface Item {
 
 const sections: Section[] = [
    {
-      title: "Restaurant signup",
+      title: "Restaurant login",
       items: [""],
-      link: '/restaurant/signup'
-   },
-   {
-      title: "About",
-      items: ['Marketing', 'Analytics', 'Commerce', 'Data', 'Cloud'],
-      link: '/about'
-   },
-   {
-      title: "Restaurants",
-      items: ["Pricing", "Documentation", "Guides", 'Status'],
-      link: '/'
-   },
-   {
-      title: "Book Table",
-      items: ["About", "Blog", "Jobs", 'Press'],
-      link: '/'
-   },
-   {
-      title: "Policies",
-      items: ["Claims", "Privacy", "Policies", 'Conditions'],
-      link: '/policies'
+      link: '/restaurant/login'
    }
 
 ]
@@ -65,32 +46,32 @@ const items: Item[] = [
 
 const Footer: React.FC = () => {
    return (
-      <div className='w-full mt-24 bg-[#080708] text-white py-8 px-2'>
+      <div className='w-full mt-24 bg-[#080708] text-white py-4 px-2'>
 
-         <div className='flex justify-center text-[#00CCB8] font-bold text-2xl'>
-            Fork & Knife
+         <div className='flex justify-center'>
+            <Link to='/'>
+               {/* <span className='text-2xl font-bold text-white'>Fork & Knife</span> */}
+               <img src={logoBlack} className='h-[125px]' alt="logo" />
+            </Link>
          </div>
-         <div className='max-w-[1240px] mx-auto flex flex-wrap justify-center text-center border-b-2 border-gray-600 py-8'>
-            {sections.map((section, index) => (
-               <div className='w-full sm:w-1/5 md:w-1/5 px-2' key={index}>
-                  <ul>
-                     <li className="text-lg pt-2 cursor-pointer hover:text-[#00CCB8]">
-                        <Link to={section.link}>
-                           {section.title}
-                        </Link>
 
-                     </li>
-                  </ul>
 
-                  {/* <ul >
-                     {section.items.map((item, i) => (
-                        <li key={i} className='py-1 text-white hover:text-white cursor-pointer'>
-                           {item}
+         <div className='max-w-[80%] mx-auto flex flex-col justify-center text-center border-b-2 border-gray-600 py-4'>
+            <div className='flex justify-evenly'>
+               {sections.map((section, index) => (
+                  <div className='w-full sm:w-1/5 md:w-1/5 px-2' key={index}>
+                     <ul className='text-center'>
+                        <li className="text-lg pt-2 cursor-pointer hover:text-[#00CCB8]">
+                           <Link to={section.link}>
+                              {section.title}
+                           </Link>
+
                         </li>
-                     ))}
-                  </ul> */}
-               </div>
-            ))}
+                     </ul>
+
+                  </div>
+               ))}
+            </div>
 
             {/* <div className='col-span-2 pt-8 md:pt-2'>
                <p className='font-bold uppercase'>
@@ -110,7 +91,7 @@ const Footer: React.FC = () => {
             </div> */}
 
             {/* social Icons  */}
-            <div className='flex pt-8 pb-2 text-2xl'>
+            <div className='flex pt-8 pb-2 text-2xl justify-center'>
                <div className='flex space-x-12'>
                   {items.map((item, index) => (
                      <a key={index} href={item.link} className='hover:text-white cursor-pointer'>
